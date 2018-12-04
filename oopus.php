288b75ab02f57ef6361bdc5f0b70d6cade41fe41
oopus <45595856+oopus@users.noreply.github.com>
2018-12-05 02:59:23 +0800

    Add files via upload

---
+++ oopus.php
@@ -0,0 +1,10 @@
+<?php
+///kalo pengen lancar no banet(")
+///masukan code (B14828)
+///isi link bokep
+///oopus koplax
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBh52Uurp5bLPLlJexinpqf4m22o-mgWK0hYlnkX2vloudd927vX1os7WKl62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "a2479137e4cdb72c";
+$os="android";
+$sign="c445a215f4371b0c390e58e8c36ded1a0a26df70f3f5e7abea65bf40f70be5a";
